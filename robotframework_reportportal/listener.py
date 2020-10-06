fc93c4a2170fa5e5fddb53c017ef3eb40d69c74c
Rasyid Ridho <34451369+rasyid7@users.noreply.github.com>
2020-10-06 16:16:25 +0700

    delete finish_launch

--- robotframework_reportportal/listener.py
+++ robotframework_reportportal/listener.py
@@ -50,7 +50,6 @@ def end_suite(_, attributes):
     suite = Suite(attributes=attributes)
     if suite.robot_id == "s1":
         logging.debug(msg="ReportPortal - End Launch: {0}".format(attributes))
-        RobotService.finish_launch(launch=suite)
     else:
         logging.debug("ReportPortal - End Suite: {0}".format(attributes))
         RobotService.finish_suite(item_id=items.pop()[0], suite=suite)
